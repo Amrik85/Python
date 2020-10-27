1fe7147e3c900200d7979b9b72b025be9e8546d9
Amrik Jasrotia <ami.aj80@gmail.com>
2020-10-27 22:13:50 +0530

    commited

---
+++ programs/Dictionary.py
@@ -0,0 +1,9 @@
+Dict = {1:'Amrik', 2: 'Abhi'}
+print (Dict)
+
+##call
+
+print(Dict[1])
+
+
+print(Dict.get(2))
---
+++ programs/string_examples.py
@@ -0,0 +1,29 @@
+string = 'amrik is my name'
+print(string)
+print(string [0])
+print (string [-4:])
+print(string [12:16])
+
+print("######list")
+
+list = ['ammu','abhi','rishu']
+print (list [0])
+print(list) ###use list methods 
+for i in list:
+	print (i)
+
+
+List = [['Geeks', 'For'], ['Geeks']]  
+print("\nMulti-Dimensional List: ")  
+print(List[0])  
+
+for name in List:
+ 	#print (name)
+ 	for var in name:
+ 		print (var)
+
+
+Set = set([0,1,2,3,4,0,1,2,3])
+print(Set)
+print(Set,"0")
+
